ed71c61e3a4d321687fd53794de97db62934d669
badralbt <84700181+badralbt@users.noreply.github.com>
2021-05-24 23:30:01 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,46 +5,46 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>EAR SEX</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://www.tumblr.com/dashboard">ART!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          GET YOUR ENERGY FROM{' '}
+          <code className={styles.code}>acid</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://www.youtube.com/" className={styles.card}>
+            <h3>Top 100 &rarr;</h3>
+            <p>Listen up.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://www.gmail.com/" className={styles.card}>
+            <h3>Contact &rarr;</h3>
+            <p>Contact me and artists.</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://www.fb.com/"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Event &rarr;</h3>
+            <p>Don't miss RAVE.</p>
+          </a>
+
+          <a
+            href="https://www.khanbank.com/mn/personal"
+            className={styles.card}
+          >
+            <h3>Deposit &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+                5111530447 Khanbank.
             </p>
           </a>
         </div>
