50cc2a243b4928c13b0317bb1903ed1b7591d65f
badralbt <84700181+badralbt@users.noreply.github.com>
2021-05-24 23:45:37 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -52,12 +52,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.instagram.com/badral.btskhn/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by BADRAL.
         </a>
       </footer>
     </div>
